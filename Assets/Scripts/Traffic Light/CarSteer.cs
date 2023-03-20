c6c718483b85020009d4a27651c5e46cc4d86a50
Kris <KrisTideyArt@gmail.com>
2023-03-20 14:04:56 +0800

    afternoon.2

--- Assets/Scripts/Traffic Light/CarSteer.cs
+++ Assets/Scripts/Traffic Light/CarSteer.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class CarSteer : MonoBehaviour
 {
+    [SerializeField] private float steerPos;
+    [SerializeField] private Image steerImage;
+
+    [SerializeField] private float leftSteerMax;
+    [SerializeField] private float rightSteerMax;
+
+    [SerializeField] EventSystem _eventSystem;
+    [SerializeField] GraphicRaycaster _graphicRaycaster;
+    private PointerEventData _pointerEventData;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +26,10 @@ public class CarSteer : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void Steering()
+    {
+
     }
 }
